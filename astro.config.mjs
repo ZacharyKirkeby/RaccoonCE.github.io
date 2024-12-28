42137c98c4b3f96b92011d6d3c5b4b25945b6afd
Zach <zakirkeby@gmail.com>
2024-12-27 18:58:35 -0600

    Revert "Hopefully fixed github actions"
    This reverts commit 1feb00ce481ff3a6fc77e83efbdea4f5c036bb28.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://raccoonce.github.io',
-    base: 'RaccoonCodeExecution',
+    site: 'RaccoonCE.github.io',
+    base: 'RaccoonCE.github.io'
 });
